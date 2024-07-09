3e0633fc205a4be02bd0556c200d522759538d44
Tech Mohan <149706492+techmohan6374@users.noreply.github.com>
2024-07-09 22:47:55 +0530

    initial commit

---
+++ resources/js/script.js
@@ -0,0 +1,21 @@
+var vm = new Vue({
+    el: '#StarGraphixApp',
+    data: {
+        openMobileNav: false,
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    watch: {
+
+    },
+    mounted() {
+
+    },
+    created() {
+
+    },
+});
