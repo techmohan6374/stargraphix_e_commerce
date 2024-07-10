fa81910a61d8f4ae403c2d175aca1c97c5691d7e
Mohanraj <mohanmano2020@gmail.com>
2024-07-10 22:20:13 +0530

    file changes done

--- resources/js/script.js
+++ resources/js/script.js
@@ -15,9 +15,9 @@ var vm = new Vue({
         updateSliderImages() {
             if (window.innerWidth >= 768) {
                 this.sliderImage = [
-                    'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Slider%20Images/1.jpg',
-                    'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Slider%20Images/1.jpg',
-                    'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Slider%20Images/1.jpg',
+                    'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Slider%20Images/1.png',
+                    'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Slider%20Images/1.png',
+                    'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Slider%20Images/1.png',
                 ];
             } else {
                 this.sliderImage = [
