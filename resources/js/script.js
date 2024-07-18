bd7e181540b4bba970ad68ec30b2a9f1f9a7f4a2
Mohanraj <mohanmano2020@gmail.com>
2024-07-18 21:20:48 +0530

    html js changes done

--- resources/js/script.js
+++ resources/js/script.js
@@ -61,42 +61,42 @@ var vm = new Vue({
             {
                 id: 1,
                 productName: 'Business Card Design',
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Business%20Cards/1.jpg',
+                productImage: '/resources/images/Designs/Business Cards/1.jpg',
                 productPrize: '500',
                 productContent: 'Get professionally crafted business card designs tailored to your brand. Impress clients with unique, high-quality designs that leave a lasting impression.',
             },
             {
                 id: 2,
                 productName: 'Flyer Design',
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Flyer/1.jpg',
+                productImage: '/resources/images/Designs/Flyer/1.jpg',
                 productPrize: '1000',
                 productContent: 'Promote your business with eye-catching flyer designs. Custom-crafted to captivate and communicate your message effectively to your audience.',
             },
             {
                 id: 3,
                 productName: 'Wedding Card Design',
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Wedding%20Cards/1.jpg',
+                productImage: '/resources/images/Designs/Wedding Cards/1.jpg',
                 productPrize: '2000',
                 productContent: 'Celebrate your special day with elegant wedding card designs. Customized to reflect your unique style and set the perfect tone for your big day.',
             },
             {
                 id: 4,
                 productName: 'Instagram Posters',
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Instagram%20Posters/1.jpg',
+                productImage: '/resources/images/Designs/Instagram Posters/1.jpg',
                 productPrize: '500',
                 productContent: 'Elevate your social media presence with our stunning and eye-catching Instagram posters. Perfect for promoting events, sales, and brand messages.'
             },
             {
                 id: 5,
                 productName: 'Resume',
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Resume/1.jpg',
+                productImage: '/resources/images/Designs/Resume/1.jpg',
                 productPrize: 350,
                 productContent: 'Stand out with our expertly crafted digital resumes, designed for both online and print use. Elevate your professional presence with tailored, visually appealing resumes.'
             },
             {
                 id: 6,
                 productName: 'Note Book',
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Note%20Book/1.jpg',
+                productImage: '/resources/images/Designs/Note Book/1.jpg',
                 productPrize: 450,
                 productContent: 'Create your unique custom notebooks with personalized designs and professional printing. Ideal for gifts, branding, and everyday use.'
             }
@@ -104,101 +104,101 @@ var vm = new Vue({
         allProductsImages: [
             {
                 id: 1,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Business%20Cards/1.jpg',
+                productImage: '/resources/images/Designs/Business Cards/1.jpg',
                 productId: 1
             },
             {
                 id: 2,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Business%20Cards/2.jpg',
+                productImage: '/resources/images/Designs/Business Cards/2.jpg',
                 productId: 1
             },
             {
                 id: 3,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Business%20Cards/3.jpg',
+                productImage: '/resources/images/Designs/Business Cards/3.jpg',
                 productId: 1,
             },
             {
                 id: 4,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Flyer/1.jpg',
+                productImage: '/resources/images/Designs/Flyer/1.jpg',
                 productId: 2,
             },
             {
                 id: 5,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Flyer/2.jpg',
+                productImage: '/resources/images/Designs/Flyer/2.jpg',
                 productId: 2
             },
             {
                 id: 6,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Flyer/3.jpg',
+                productImage: '/resources/images/Designs/Flyer/3.jpg',
                 productId: 2
             },
             {
                 id: 7,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Flyer/4.jpg',
+                productImage: '/resources/images/Designs/Flyer/4.jpg',
                 productId: 2
             },
             {
                 id: 8,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Flyer/5.jpg',
+                productImage: '/resources/images/Designs/Flyer/5.jpg',
                 productId: 2
             },
             {
                 id: 9,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Flyer/6.jpg',
+                productImage: '/resources/images/Designs/Flyer/6.jpg',
                 productId: 2
             },
             {
                 id: 10,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Instagram%20Posters/1.jpg',
+                productImage: '/resources/images/Designs/Instagram Posters/1.jpg',
                 productId: 4
             },
             {
                 id: 11,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Instagram%20Posters/2.jpg',
+                productImage: '/resources/images/Designs/Instagram Posters/2.jpg',
                 productId: 4
             },
             {
                 id: 12,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Instagram%20Posters/3.jpg',
+                productImage: '/resources/images/Designs/Instagram Posters/3.jpg',
                 productId: 4
             },
             {
                 id: 13,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Resume/1.jpg',
+                productImage: '/resources/images/Designs/Resume/1.jpg',
                 productId: 5
             },
             {
                 id: 14,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Resume/2.jpg',
+                productImage: '/resources/images/Designs/Resume/2.jpg',
                 productId: 5
             },
             {
                 id: 15,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Resume/3.jpg',
+                productImage: '/resources/images/Designs/Resume/3.jpg',
                 productId: 5
             },
             {
                 id: 16,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Note%20Book/1.jpg',
+                productImage: '/resources/images/Designs/Note Book/1.jpg',
                 productId: 6
             },
             {
                 id: 17,
-                productImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Designs/Note%20Book/2.jpg',
+                productImage: '/resources/images/Designs/Note Book/2.jpg',
                 productId: 6
             }
         ],
         websiteProducts: [
             {
                 id: 1,
-                websiteImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Websites/Web_1.jpg',
+                websiteImage: '/resources/images/Websites/Web_1.jpg',
                 websiteType: 'Web Application',
                 websiteName: 'Invoice Generation Website',
                 websiteContent: 'Effortlessly create and manage invoices with our user-friendly platform. Generate professional invoices, download them as PDFs, and track your invoicing activity through a comprehensive dashboard.'
             },
             {
                 id: 2,
-                websiteImage: 'https://raw.githubusercontent.com/techmohan6374/stargraphix_e_commerce/main/resources/images/Websites/Web_2.jpg',
+                websiteImage: '/resources/images/Websites/Web_2.jpg',
                 websiteType: 'Website',
                 websiteName: 'Portfolio Website',
                 websiteContent: 'A professional portfolio website using HTML, CSS, and JS, it has smooth page transitions, cool background effects, a unique design, and it is mobile responsive. It showcases my skills, projects, and experience in a visually engaging manner.'
@@ -270,7 +270,8 @@ var vm = new Vue({
             elem.forEach((carousel) => {
                 new Flickity(carousel, {
                     cellAlign: 'center',
-                    contain: true
+                    contain: true,
+                    autoPlay: 2000
                 });
             });
         },
